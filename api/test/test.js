938208c934b22e1535c440145c665c547cdfb687
unknown <cross29123@hotmail.com>
2020-01-17 21:25:08 -0300

    try add console.log to see errors

--- api/test/test.js
+++ api/test/test.js
@@ -22,6 +22,7 @@ describe("Testing auth system", () => {
       .send(user)
       .end((err, res) => {
         expect(res.status).to.equal(201);
+        console.log(err);
         done();
       });
   });
@@ -37,6 +38,7 @@ describe("Testing auth system", () => {
       .set("Accept", "application/json")
       .send(user)
       .end((err, res) => {
+        console.log(err);
         expect(res.status).to.equal(200);
         AUTH_TOKEN = res.body.data;
         done();
@@ -53,6 +55,7 @@ describe("Testing the get all GalleryPics endpoind:", () => {
       .set("Authorization", AUTH_TOKEN)
       .end((err, res) => {
         expect(res.status).to.equal(200);
+        console.log(err);
         done();
       });
   });
@@ -64,6 +67,7 @@ describe("Testing the get all GalleryPics endpoind:", () => {
       .set("Accept", "application/json")
       .set("Authorization", AUTH_TOKEN)
       .end((err, res) => {
+        console.log(err);
         const { title, description, imageUrl } = res.body.data;
         expect(res.status).to.equal(200);
         expect(title).to.be.a("string").that.is.not.empty;
