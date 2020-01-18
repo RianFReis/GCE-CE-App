4481b948e871670a9bc5eeca2792c9b946ff6285
unknown <cross29123@hotmail.com>
2020-01-18 18:04:21 -0300

    fix try

--- api/test/test.js
+++ api/test/test.js
@@ -1,8 +1,1 @@
-import chai from "chai";
-import chatHttp from "chai-http";
-import "chai/register-should";
-import app from "../index";
 
-chai.use(chatHttp);
-const { expect } = chai;
-var AUTH_TOKEN = null;
